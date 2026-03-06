6ca2aa05358766b7b0a02c005f1fac9d652b4d13
maheshkokate07 <kokatemahesh2004@gmail.com>
2026-03-07 00:09:25 +0530

    Settings panel modify - Test mode badge

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -101,6 +101,10 @@ export default function Header({
 
                {/* Right */}
                <div className="flex items-center justify-end gap-2.5 flex-1 h-10">
+                    {/* <Badge variant="destructive" className="flex items-center h-6 gap-1.5">
+                         <CircleAlert />
+                         Test mode
+                    </Badge> */}
                     {/* <ThemeToggle /> */}
                     <SettingsPanel headerHeight={headerHeight} />
                </div>
--- src/components/custom/SettingsPanel.tsx
+++ src/components/custom/SettingsPanel.tsx
@@ -9,12 +9,14 @@ import {
      SheetTitle,
      SheetTrigger,
 } from "@/components/ui/sheet"
-import { Settings } from "lucide-react"
+import { CircleAlert, Dot, Settings } from "lucide-react"
 import { Switch } from "../ui/switch"
 import { useEffect, useState } from "react"
 import { useTheme } from "@/providers/ThemeProvider"
 import { useAppDispatch, useAppSelector } from "@/store/hooks"
 import { setActiveNetwork } from "@/slices/appSlice"
+import { Badge } from "../ui/badge"
+import { toast } from "sonner"
 
 export default function SettingsPanel({ headerHeight }: { headerHeight: string }) {
      const dispatch = useAppDispatch();
@@ -42,18 +44,28 @@ export default function SettingsPanel({ headerHeight }: { headerHeight: string }
      const handleNetworkChange = (value: boolean) => {
           setIsDevMode(value);
           dispatch(setActiveNetwork(!!value ? 'DEVNET' : 'MAINNET'));
+          toast(!!value ? "You're now in Test mode." : "Switched back to Normal mode.")
      }
 
      return (
           <Sheet>
                <SheetTrigger asChild>
-                    <Button variant="outline" className="cursor-pointer h-10">
-                         <Settings className="scale-110 sm:scale-130 opacity-90" />
+                    <Button variant="outline" className="cursor-pointer h-10 relative">
+                         <Settings className="scale-115 sm:scale-130 opacity-90" />
+                         {activeNetwork === 'DEVNET' && <div className="bg-background rounded-full group absolute flex h-3.5 w-3.5 items-center justify-center bottom-7 left-7.5">
+                              <Dot className="text-red-500 scale-340 sm:scale-380" />
+                         </div>}
                     </Button>
                </SheetTrigger>
                <SheetContent showCloseButton={false}>
                     <SheetHeader className="border-b flex justify-center" style={{ height: headerHeight }}>
-                         <SheetTitle className="text-lg">App settings</SheetTitle>
+                         <SheetTitle className="text-lg flex items-center justify-between">
+                              App settings
+                              {activeNetwork === 'DEVNET' && <Badge variant="destructive" className="flex items-center gap-1">
+                                   <CircleAlert style={{ marginTop: '1px' }} />
+                                   Test mode
+                              </Badge>}
+                         </SheetTitle>
                          {/* <SheetDescription>
                               Make changes to your profile here. Click save when you&apos;re done.
                          </SheetDescription> */}
